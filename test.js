e125486c5eaae4a9e330b7e1cc72150700d26ad7
zhanglu.bd <zhanglu.bd@bytedance.com>
2019-05-18 17:02:32 +0800

    feat: 添加测试脚本
    Change-Id: I2f134b5b812de5574dc9fbfc2b5e9e897c88bf8c

---
+++ test.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         sen_label
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  敏感数据标注
+// @author       madeira zhanglu
+// @match        *://github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    // 代码行
+    var code_line = document.createElement('textarea')
+    code_line.innerHTML = '<textarea id="nums" rows="2"></textarea>';
+
+    // 正常按钮
+    var button1 = document.createElement('button');
+    button1.innerHTML = '<button id="button1">确认</button>';
+
+    // 敏感按钮
+    var button2 = document.createElement('button');
+    button2.innerHTML = '<button id="button2">取消</button>';
+
+    document.body.appendChild(nums);
+    document.body.appendChild(button1);
+    document.body.appendChild(button2);
+
+    //--- Activate the newly added button.
+    document.getElementById("button1").addEventListener (
+        "click", ButtonClickAction, false
+    );
+
+function ButtonClickAction (zEvent) {
+    ;
+}
+})();
